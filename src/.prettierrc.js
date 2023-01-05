9ce0f8b056dd9afb9322462ff3b921294225dbd6
isaurssaurav <saurssaurav33@gmail.com>
2023-01-05 23:16:27 +0100

    init - setup folder structure

---
+++ src/.prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  insertPragma: false,
+  singleAttributePerLine: false,
+  bracketSameLine: false,
+  jsxBracketSameLine: false,
+  printWidth: 120,
+  proseWrap: "preserve",
+  requirePragma: false,
+  semi: true,
+  trailingComma: "es5",
+  useTabs: false,
+};
---
+++ src/Hello.jsx
@@ -0,0 +1,3 @@
+const Hello = () => <h1>hello world from react</h1>;
+
+export default Hello;
